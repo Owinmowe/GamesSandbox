eb9981f78250f4bea964e29da5a6224e26407aa9
Adrian Sgro <Owinmowe@gmail.com>
2023-08-23 17:01:57 -0300

    Improve control screens comments

--- Assets/Scripts/BottleGame/UI/Controls/StartScreenControl.cs
+++ Assets/Scripts/BottleGame/UI/Controls/StartScreenControl.cs
@@ -6,17 +6,17 @@ using General.MVP;
 namespace BottleGame.UI.Controls
 {
     /// <summary>
-    /// Control class of the BottleGame. This class implements IScreenControl interface
+    /// Control class of the BottleGame. This class implements IScreenControl interface.
     /// </summary>
     public class StartScreenControl : MonoBehaviour, IScreenControl
     {
-        /// <summary>Event called from StartScreenControl when exit button is pressed</summary>
+        /// <summary>Event called from StartScreenControl when exit button is pressed.</summary>
         public event Action OnExitButtonPressed;
         
-        /// <summary>Event called from StartScreenControl when start button is pressed</summary>
+        /// <summary>Event called from StartScreenControl when start button is pressed.</summary>
         public event Action OnStartButtonPressed;
         
-        /// <summary>Event called from StartScreenControl when settings button is pressed</summary>
+        /// <summary>Event called from StartScreenControl when settings button is pressed.</summary>
         public event Action OnSettingsButtonPressed;
         
         [Header("Buttons")]
@@ -42,10 +42,10 @@ namespace BottleGame.UI.Controls
         private void SettingsButtonPressed() => OnSettingsButtonPressed?.Invoke();
         private void ExitButtonPressed() => OnExitButtonPressed?.Invoke();
         
-        /// <summary>Method for enabling the StartScreen gameObject</summary>
+        /// <summary>Method for enabling the StartScreen gameObject.</summary>
         public void OpenScreen() => gameObject.SetActive(true);
         
-        /// <summary>Method for disabling the StartScreen gameObject</summary>
+        /// <summary>Method for disabling the StartScreen gameObject.</summary>
         public void CloseScreen() => gameObject.SetActive(false);
     }
 }
