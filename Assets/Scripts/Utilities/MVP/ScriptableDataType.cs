8417d97b12aa800b9febcb9cadfde452701949b1
Adrian Sgro <Owinmowe@gmail.com>
2023-08-23 10:16:13 -0300

    Add GameplayScreen and StartScreen Data with new Enum Identifier

---
+++ Assets/Scripts/Utilities/MVP/ScriptableDataType.cs
@@ -0,0 +1,11 @@
+namespace Utilities.MVP
+{
+    /// <summary>
+    /// Enum used to differentiate data from different screens forced on all ScriptableData
+    /// </summary>
+    public enum ScriptableDataType
+    {
+        StartScreenData,
+        GameplayScreenData
+    }
+}
